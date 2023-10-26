fac8ddf7b8088c5e42abfb2a05619f5ac231ff3e
Sankalp Haritash <110713125+SankalpHaritash21@users.noreply.github.com>
2023-10-26 08:40:01 +0530

    socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,7 @@
 import { io } from "socket.io-client";
+const url =
+  process.env.NODE_ENV === "production"
+    ? "https://scratch-book-server.onrender.com"
+    : "http://localhost:5000";
 
-export const socket = io("http://localhost:5000");
+export const socket = io(url);
